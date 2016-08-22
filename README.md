3d21cc5915f08039169588f82bd6b3ac3d012a33
Mohamed Mehdi SAAD <mohamed-mehdi.saad@viseo.com>
2016-08-22 20:43:52 +0200

    first refactor & add comments

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Kata-String-Calculator-Java-8
-•	null => 0
-•	"" => 0
-•	"0" => 0
-•	"1" => 1
-•	"1+2" => 3
-•	"2+4-1" => 5
-•	"-10+3+1-2" => -8
-•	"3++5--2" => 6 (le séparateur / opérateur peut-être doublé sans impact)
+This project, take a List of number in a String for example "1+5-2" and do the math, for this String input it should return 4.
+
+>	null => 0
+>	"" => 0
+>	"0" => 0
+>	"1" => 1
+>	"1+2" => 3
+>	"2+4-1" => 5
+>	"-10+3+1-2" => -8
+>	"3++5--2" => 6 (le séparateur / opérateur peut-être doublé sans impact)
 
 
--- src/main/java/com/Viseo/KataStringCalculator/KataStringCalculator.java
+++ src/main/java/com/Viseo/KataStringCalculator/KataStringCalculator.java
@@ -2,6 +2,7 @@ package com.Viseo.KataStringCalculator;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by MSA3513 on 22/08/2016.
@@ -15,10 +16,15 @@ class KataStringCalculator {
         if (numbers == null || numbers.length() < 1) {
             return 0;
         }
+
+        //Edit the numbers to add, in the method getValBySign we should have the first char as a sign(+ or -) and end
+        //with a non numeric value.
         String rectifiedNumbers = Character.isDigit(numbers.charAt(0)) ? "+" + numbers + "+" : numbers + "+";
 
-        getValBySign(rectifiedNumbers, "+").forEach(a -> sum += Integer.parseInt(a));
-        getValBySign(rectifiedNumbers, "-").forEach(a -> sum -= Integer.parseInt(a));
+        Consumer<String> plus = a -> sum += Integer.parseInt(a);
+        Consumer<String> minus = a -> sum -= Integer.parseInt(a);
+        getValBySign(rectifiedNumbers, "+").forEach(plus);
+        getValBySign(rectifiedNumbers, "-").forEach(minus);
 
         return sum;
     }
@@ -26,7 +32,11 @@ class KataStringCalculator {
     private static List<String> getValBySign(String s, String separator) {
         List<String> allVal = new LinkedList<>();
         for (int i = 1; i < s.length(); i++) {
+            //to verify whether our number is + or minus, we check the char before this actual number
+            //if it equal our separator then we add it to our list.
             if (Character.isDigit(s.charAt(i)) && Character.toString(s.charAt(i - 1)).equals(separator)) {
+                //check the first numeric val, if the next char is numeric we concatenate both number
+                //else we add to our list a one letter number.
                 if (Character.isDigit(s.charAt(i)) && Character.isDigit(s.charAt(i + 1))) {
                     allVal.add(Character.toString(s.charAt(i)) + s.charAt(i + 1));
                     i++;
@@ -36,10 +46,5 @@ class KataStringCalculator {
             }
         }
         return allVal;
-    }
-
-
-    private List<Integer> getNegatif(String s) {
-        return null;
     }
 }
